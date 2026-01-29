9b27292afe20551913660671ca67905a864ca089
Kushal D S <kushalkush132003@gmail.com>
2026-01-29 21:24:21 +0530

    Add SquareArea class to calculate square area

---
+++ square.java
@@ -0,0 +1,12 @@
+public class SquareArea {
+    public static void main(String[] args) {
+        // Define the side length of the square
+        int side = 5; // you can change this value
+
+        // Calculate the area
+        int area = side * side;
+
+        // Display the result
+        System.out.println("The area of the square with side " + side + " is: " + area);
+    }
+}
