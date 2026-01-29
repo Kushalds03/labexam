b71ac5d88f62c442d0858d845709c21dfedabc2a
Kushal D S <kushalkush132003@gmail.com>
2026-01-29 22:16:43 +0530

    Update SI.java

--- SI.java
+++ SI.java
@@ -2,7 +2,7 @@ public class SimpleInterest {
     public static void main(String[] args) {
         // Define the principal, rate of interest, and time
         double principal = 10000; // Principal amount
-        double rate = 5;          // Annual interest rate in percent
+        double rate = 4;          // Annual interest rate in percent
         double time = 2;          // Time in years
 
         // Calculate simple interest
